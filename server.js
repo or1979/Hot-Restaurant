0575df3551a596dddbaf601b39d20debc7da0e24
motkilo <code22@motkilo.com>
2019-04-16 14:09:11 -0400

    added server stuff

---
+++ server.js
@@ -0,0 +1,16 @@
+// Dependencies
+var express = require("express");
+var path = require("path");
+
+// Set up Express App
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Express app data parsing
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//Start server to beging listening
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
