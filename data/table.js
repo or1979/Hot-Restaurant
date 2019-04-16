e6ca873751246da9caa8e0924cc63408218e93e4
or1979 <orobinson79@gmail.com>
2019-04-16 14:04:06 -0400

    first

---
+++ data/table.js
@@ -0,0 +1,11 @@
+var tableArray = [
+    {
+      customerName: "Jared",
+      customerEmail: "jared@example.com",
+      customerID: "afhaque89",
+      phoneNumber: "240-555-5992"
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = tableArray;
